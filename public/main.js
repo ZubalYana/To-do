13d265d3908c10abf5335e5314b3be68569d2788
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-20 14:34:17 +0300

    display the tasks and style the input

---
+++ public/main.js
@@ -0,0 +1,25 @@
+axios.get('http://localhost:3000/tasks')
+.then((res)=>{
+    console.log(res.data);
+    const tasks = res.data;
+    for(let task of tasks){
+        $('#taskContainer').append(
+            `<div class="task">
+            <div class="task_title">${task.title}</div>
+            <div class="task_description"></div>
+        </div>`
+        )
+    }
+})
+
+
+$('#addTask').click(()=>{
+    let data = {
+        title: $('#task').val()
+    }
+    axios.post('http://localhost:3000/add-task', data)
+    .then(res=>{
+        console.log(res.data);
+        location.href = '/';
+    })
+})

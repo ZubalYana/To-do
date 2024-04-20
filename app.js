0cc2fab11c43402467ceae92b9e8436218e6ecd7
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-20 13:57:26 +0300

    connect mongoDB

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 const path = require('path');
 const PORT = 3000;
+
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect(`mongodb+srv://root:CdLeejqYl0BDpKLE@cluster0.raizszz.mongodb.net/?retryWrites=true&w=majority`)
+.then(()=>{
+    console.log(`Connected to mongoDB`)
+})
+
+const Task = mongoose.model({
+    title: String
+})
+app.post('add-task', async (req, res)=>{
+    try{
+        const {title} = req.body;
+        const task = new Task({title});
+        await task.save();
+        res.status(201).json(task);
+    }catch(err){
+        res.status(500).json({message: err});
+    }
+
+})
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-
 })
 
 app.listen(PORT, ()=>{
